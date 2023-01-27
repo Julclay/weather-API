40cca124724fcebd1334402677919c73739ad4d7
Julien Claivaz <julienclaivaz@juliens-mbp.home>
2023-01-27 19:58:50 +0100

    final cahnges

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ let cityTitle = document.querySelector("#city-name")
 let wind = document.querySelector("#wind")
 let temp = document.querySelector("#temp")
 let humidity = document.querySelector("#humidity")
+let today = moment()
 
 
-searchButton.addEventListener("click", function(event) {
+searchButton.addEventListener("click", function search(event) {
 
     event.preventDefault();
 
@@ -31,7 +32,11 @@ searchButton.addEventListener("click", function(event) {
 
         historyDiv.appendChild(historyButton);
 
+
+
         return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=107a7d6c443c023117142972cf673c10`)
+
+       
     })
 
     .then(response => response.json())
@@ -41,14 +46,17 @@ searchButton.addEventListener("click", function(event) {
 
         temp.textContent = "temp: " +  Math.round(data.list[0].main.temp - 273.15) + "C"
         wind.textContent = "wind: " + data.list[0].wind.speed + "KPH"
-        cityTitle.textContent = cityName
+        cityTitle.textContent = cityName + " (" + today.format("D/MMM/YYYY") + ")"
         humidity.textContent = "humidity: " + data.list[0].main.humidity + "%"
 
-
+     
 
     })
 
+ 
+    
 })
+
 
 
 
